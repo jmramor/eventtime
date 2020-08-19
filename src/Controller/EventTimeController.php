a1267ce9d5c0557d90687dd93f47af958f4d285f
jmramor <jernejmramor@gmail.com>
2020-08-19 10:42:36 +0200

    Initial commit

---
+++ src/Controller/EventTimeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\eventtime\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Defines EventTimeController class.
+ */
+class EventTimeController extends ControllerBase {
+
+  /**
+   * Display the markup.
+   *
+   * @return array
+   *   Return markup array.
+   */
+  public function content() {
+    return [
+      '#type' => 'markup',
+      '#markup' => $this->t('Hello, World!'),
+    ];
+  }
+
+}
